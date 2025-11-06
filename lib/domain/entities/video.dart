0515ebbf813494774f9435b4ef091d901d214bb2
rolyalc <rolalcedu@gmail.com>
2025-11-06 13:06:53 +0100

    change(video): pequeños cambios en la entidad video

--- lib/domain/entities/video.dart
+++ lib/domain/entities/video.dart
@@ -1,27 +1,20 @@
 class Video {
-  String id;
-  String? topic;
-  String? description;
-  num? duration;
-  String? thumbnail;
+  final String id;
+  final String topic;
+  final String? description;
+  final double? duration;
+  final String? thumbnail;
 
-  Video(
-      {
-        required this.id,
-        this.topic,
-        this.description,
-        this.duration,
-        this.thumbnail
-      });
+  Video({
+    required this.id,
+    required this.topic,
+    this.description,
+    this.duration,
+    this.thumbnail,
+  });
 
   @override
   String toString() {
-    return '''
-    \x1B[34mid:\t\t\x1B[36m$id\n\x1B[0m
-    \x1B[34mtopic:\t\x1B[36m$topic\n\x1B[0m
-    \x1B[34mdescription:\t\x1B[36m$description\n\x1B[0m
-    \x1B[34mduration:\t\t\x1B[36m$duration\n\x1B[0m
-    \x1B[34mthumbnail:\t\x1B[36m$thumbnail\n\x1B[0m
-    ''';
+    return 'Video(id: $id, topic: $topic, duration: $duration)';
   }
 }
