d5e12df7cd2b2a6e2b28c860141e4b3cd18515c9
rolyalc <rolalcedu@gmail.com>
2025-11-07 10:07:24 +0100

    add(repository): añadido infraestructura del repository

---
+++ lib/infrastructure/repository/videos_repository.dart
@@ -0,0 +1,7 @@
+import 'package:justflix_frontend/domain/entities/video.dart';
+import 'package:justflix_frontend/domain/entities/video_simple.dart';
+
+abstract class VideosRepository {
+  Future<List<VideoSimple>> getVideos(String searchQuery);
+  Future<Video?> getVideoById(String videoId);
+}
