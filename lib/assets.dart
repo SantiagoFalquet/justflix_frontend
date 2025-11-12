865fbdc015a5b65ff226ccbe38c53242caceaee6
rolyalc <rolalcedu@gmail.com>
2025-11-12 01:22:56 +0100

    add(assets.dart) establecer ruta jerarquica de assets

---
+++ lib/assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  /// Imágenes
+  static const _baseImages = 'assets/images/';
+  static const images = _Images();
+}
+
+class _Images {
+  const _Images();
+  final String logo = '${AppAssets._baseImages}/logo_justflix.png';
+}
+/// Futoro: Añadiro Fuentes, Iconos, Animaciones ...
