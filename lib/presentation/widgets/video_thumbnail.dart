9a3e3c667b459f010732a52103ca8b51c4804c6a
rolyalc <rolalcedu@gmail.com>
2025-11-18 23:00:31 +0100

    mod,doc(video_thumbnail): crear metodos widget para separar code

--- lib/presentation/widgets/video_thumbnail.dart
+++ lib/presentation/widgets/video_thumbnail.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
 
 /// Muestra la miniatura de un vídeo.
-/// 
-/// - sI [url] es válida, carga la img desde la red.
+///
+/// - Si [thumbnailUrl] es válida, carga la img desde la red.
 /// - Si la imagen tarda en cargar, muestra un indicador de progreso [CircularProgressIndicator].
-/// - Si falla la carga o [url] es nula/vacia, meustra un icono por defecto [Icons.broken_image].
-/// 
+/// - Si falla la carga o [thumbnailUrl] es nula/vacia, muestra un icono por defecto [Icons.broken_image].
+///
 class VideoThumbnail extends StatelessWidget {
   /// URL de la miniatura del video.
-  final String? url;
-  const VideoThumbnail({super.key, this.url});
+  final String? thumbnailUrl;
+
+  /// Ancho y alto del widget.
+  final double width;
+  final double height;
+
+  const VideoThumbnail({
+    super.key,
+    this.thumbnailUrl,
+    this.width = 80.0,
+    this.height = 80.0,
+  });
 
   @override
   Widget build(BuildContext context) {
-    if (url != null && url!.isNotEmpty) {
+    if (thumbnailUrl != null && thumbnailUrl!.isNotEmpty) {
       return Image.network(
-        url!,
+        thumbnailUrl!,
         fit: BoxFit.cover,
-        width: 80,
-        height: 80,
+        width: width,
+        height: height,
         // Muestra un indicador de carga mientras la imagen se descarga
         loadingBuilder: (context, child, loadingProgress) {
           if (loadingProgress == null) return child;
@@ -26,12 +36,34 @@ class VideoThumbnail extends StatelessWidget {
         },
         // Muestra un icono de error si la imagen no se puede cargar
         errorBuilder: (context, error, stackTrace) {
-          return const Icon(Icons.broken_image, size: 40);
+          print('[ERROR] - Al cargar la imagen: $error');
+          return _buildErrorPlaceholder();
         },
       );
     } else {
       // Si no hay thumbnail, muestra un icono de película
-      return const Icon(Icons.movie, size: 40);
+      return _peliculaPlaceholder();
     }
+  }
+
+  /// Widget de reemplazo que muestra un icono de error si la imagen no se puede cargar.
+  /// 
+  Widget _buildErrorPlaceholder() {
+    return SizedBox(
+        width: width,
+        height: height,
+        child: const Icon(Icons.broken_image, size: 40),
+      );
+  }
+  
+  /// Widget de reemplazo que si no hay thumbnail, muestra un icono de película.
+  /// 
+  Widget _peliculaPlaceholder() {
+    return Container(
+        width: width,
+        height: height,
+        color: Colors.grey[300],
+        child: const Icon(Icons.movie, size: 40, color: Colors.grey,),
+      );
   }
 }
