0605b3bfb5f5e09e511a238eb00c93dcb3db2c37
rolyalc <rolalcedu@gmail.com>
2025-11-18 21:56:56 +0100

    add(formatters): Separación y reutilización para formatear tiempo

---
+++ lib/utils/formatters.dart
@@ -0,0 +1,17 @@
+/// Formatea una duración en segundos a un formato de cadena 'mm:ss'.
+/// Devuelve 'N/A' si [seconds] es nulo, negativo o no válido.
+///
+String formatoDuration(double? seconds) {
+  // Validación
+  if (seconds == null || seconds < 0) {
+    return 'N/A';
+  }
+  // Convertir los segundos a un objeto Duration
+  final duration = Duration(seconds: seconds.toInt());
+
+  // Obtener minutos y segundos, asegurando 2 dígitos con padLeft
+  final minutes = duration.inMinutes.remainder(60).toString().padLeft(2, '0');
+  final secs = duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+
+  return '$minutes:$secs';
+}
