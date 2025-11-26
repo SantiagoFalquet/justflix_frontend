46e04791fe2201597e54832f5dccc7847ba43953
rolyalc <rolalcedu@gmail.com>
2025-11-26 11:27:01 +0100

    documentar - cambios menores

--- lib/app.dart
+++ lib/app.dart
@@ -7,32 +7,45 @@ import 'package:justflix_frontend/presentation/providers/videos_providers.dart';
 import 'package:justflix_frontend/presentation/screens/home/home_screen.dart';
 import 'package:justflix_frontend/infrastructure/config/app_config.dart';
 
+/// Widget raíz de la aplicación.
+///
+/// Se encarga de preparar toda la estructura inicial antes de mostrar la UI.
+/// Se configura los proveedores necesarios para obtener datos, acceder
+/// al repositorio de videos y manejar los esatdo de la aplicación.
+///
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // URL base de la API de flutter + puerto (.env)
     final String apiBaseUrl = AppConfig.apiUrl;
+
     return MultiProvider(
       providers: [
-        // Proveedor para la capa de datos
+        // Proveedor para la capa de datos. Crea la instancia q se comunica
+        // directamente con la API remota
         Provider<VideosApi>(create: (_) => VideosApi(apiBaseUrl)),
 
-        // Proveedor para la capa de repositorio
+        // Proveedor para la capa de repositorio.
+        // Capa intermedia entra la API y la lógica de la aplicación
         ProxyProvider<VideosApi, VideosRepository>(
           update: (_, api, __) => VideosRepositoryImpl(api),
         ),
-        // Proveedor de estado para la UI
+
+        // Proveedor de estado que gestiona la lógica usadaen la UI.
         ChangeNotifierProvider(
           create: (context) =>
               VideosProvider(videosRepository: context.read<VideosRepository>())
                 ..loadVideos(), // Cargamos los vídeos al iniciar la app
         ),
       ],
+
+      // Aplicación principal de Flutter
       child: MaterialApp(
         title: 'Justflix',
         debugShowCheckedModeBanner: false,
-        home: HomeScreen(), // Usamos la HomeScreen
+        home: HomeScreen(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:justflix_frontend/app.dart';
 
+/// Entrada principal de la aplicación.
+/// 
+/// Su función principal es
+///   Cargar las variables de entorno antes de iniciar la app Flutter
+/// Una vez cargadas, se ejecuta el widget raiz `MyApp`
+/// 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
+  
   runApp(const MyApp());
 }
